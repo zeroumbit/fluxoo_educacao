b6a07e228e527eaae9d0c50550cd74ce3acf7dab
ROPE Dev Team <contato@espaco-z.com>
2026-03-10 07:35:51 -0300

    Merge remote changes

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -85,7 +85,7 @@ define(['./workbox-3f1afc6c'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.2q9ag0nj0gg"
+    "revision": "0.lskgsijkds8"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
