6d0319a65057f1a004fbf38bda33613c276cc1fd
ROPE Dev Team <contato@espaco-z.com>
2026-03-13 08:15:49 -0300

    chore: update PWA service worker revision

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -85,7 +85,7 @@ define(['./workbox-3f1afc6c'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.5h0001r0a5o"
+    "revision": "0.qhtp5dbo5t"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
