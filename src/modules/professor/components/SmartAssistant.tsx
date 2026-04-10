14b9ba76dc88d44945662c619bfea9cd388800a2
Seu Nome <seu@email.com>
2026-04-10 20:59:13 -0300

    fix: corrige posição do FAB do Assistente Inteligente (IA) no layout do professor

--- src/modules/professor/components/SmartAssistant.tsx
+++ src/modules/professor/components/SmartAssistant.tsx
@@ -120,7 +120,7 @@ export function SmartAssistant({
   return (
     <>
       {/* FAB - Botão Flutuante */}
-      <div className="fixed bottom-6 right-6 z-50">
+      <div className="fixed bottom-28 right-6 z-30">
         <button
           onClick={() => handleOpenChange(true)}
           className={cn(
